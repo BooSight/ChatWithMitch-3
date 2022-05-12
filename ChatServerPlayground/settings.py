db57312a4641ed8e4cb0e3afb1ea0f0e3f77c346
har0armenak <har0armenak@gmail.com>
2022-05-12 17:48:46 +0400

    registration

--- ChatServerPlayground/settings.py
+++ ChatServerPlayground/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from telnetlib import AUTHENTICATION
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -28,6 +29,10 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 AUTH_USER_MODEL = "account.Account"
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.AllowAllUsersModelBackend',
+    'account.backends.CaseInsensitiveModelBackend'
+)
 
 # Application definition
 
@@ -79,7 +84,7 @@ WSGI_APPLICATION = 'ChatServerPlayground.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DB_NAME = "codingwithmitch_chat_dev"
+DB_NAME = "postgres"
 DB_USER = "django"
 DB_PASSWORD = "password"
 DATABASES = {
